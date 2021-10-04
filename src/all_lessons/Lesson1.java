54d3a0fa1d9f553e2f8067c31d573989a957e288
RanoJ15 <ranojuraeva78@gmail.com>
2021-10-04 10:50:18 -0400

    New file lesson1 Update file lesson1 Added lesson1

--- src/all_lessons/Lesson1.java
+++ src/all_lessons/Lesson1.java
@@ -4,6 +4,7 @@ public class Lesson1 {
     public static void main(String[] args) {
         System.out.println("Hello Kamila");
         System.out.println("Hello Rano");
+        System.out.println("Hello everyone");
 
     }
 
